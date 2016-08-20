5d1745253c89d6ae07dc776052a1858c25ecc487
spitz-dan-l <spitz.dan.l@gmail.com>
2016-08-20 16:19:47 -0400

    refactor

--- pythonKeyBoard.py
+++ pythonKeyBoard.py
@@ -1,34 +1,46 @@
 import serial, time, msvcrt
 
-arduinoSerial = serial.Serial('com3', 9600, timeout=0.1);
-time.sleep(2);
-tmpData = 0.0;
+arduinoSerial = serial.Serial('com3', 9600, timeout=0.1)
+time.sleep(2)
+tmpData = 0.0
 
-while(1):
-	print("Press [W, A, S, or D]\n");
-	
-	rawKey = msvcrt.getch();
-	
-	key = rawKey.decode();
-	print("You pressed: " + key);
+import struct
 
-	if(key == "A"):
-		arduinoSerial.write('1'.encode());
-	elif(key == "D"):
-		arduinoSerial.write('2'.encode());
-	elif(key == "W"):
-		arduinoSerial.write('3'.encode());
-	elif(key == "S"):
-		arduinoSerial.write('4'.encode());
-	else:
-		arduinoSerial.write('0'.encode());
-		
-	if(key == "0"):
-		break;
-		
-	if(arduinoSerial.inWaiting() > 0):
-		tmpData = arduinoSerial.readline();
-		print(float (tmpData));
 
-arduinoSerial.close();
-print("Program terminated");
+def make_instruction(x, y):
+    return struct.pack('Bbb', 2, x, y)
+
+def main():
+    while(True):
+        print "Press [W, A, S, or D]\n"
+        
+        rawKey = msvcrt.getch()
+        
+        key = rawKey.decode()
+        print "You pressed: " + key
+
+        if(key == "A"):
+            ins = make_instruction(-1, 0)
+        elif(key == "D"):
+            ins = make_instruction(1, 0)
+        elif(key == "W"):
+            ins = make_instruction(0, 1)
+        elif(key == "S"):
+            ins = make_instruction(0, -1)
+        else:
+            ins = make_instruction(0, 0)
+
+        arduinoSerial.write(ins)
+
+        if(key == "0"):
+            break
+            
+        if(arduinoSerial.inWaiting() > 0):
+            tmpData = arduinoSerial.readline()
+                print float(tmpData)
+
+    arduinoSerial.close()
+    print "Program terminated"
+
+if __name__ == '__main__':
+    main()
